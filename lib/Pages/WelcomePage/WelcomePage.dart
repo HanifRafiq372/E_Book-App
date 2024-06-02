add1bc3e9aaf0bbee8369b3aec6e601b070b1f46
Maulana Rafi <maulanarafinurdiansyah22@gmail.com>
2024-06-03 03:35:51 +0700

    yt menit 48:14 WelcomePage.dart

--- lib/Pages/WelcomePage/WelcomePage.dart
+++ lib/Pages/WelcomePage/WelcomePage.dart
@@ -1,16 +1,32 @@
 import 'package:flutter/material.dart';
 
-class WelcomePage extends StatelessElement {
+class WelcomePage extends StatelessWidget {
   const WelcomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [Text("Welcome Page")],
-        ),
-      ),
-    );
+      body: Column(
+    children: [
+        Container(
+          height: 500,
+          color: Theme.of(context).colorScheme.primary,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                "Assets/Images/book.png",
+                width: 380,
+                ),
+                SizedBox(height: 60),
+                Text("E - Book Store",style: Theme.of(context).textTheme.headlineLarge?.copyWith(color: Theme.of(context).colorScheme.background,
+                ),),],
+              )
+            ]),
+          )
+        ],
+      ));
   }
 }
