92f4b11feceffa85aa219121b1ee4e7f0adf7d2b
Maulana Rafi <maulanarafinurdiansyah22@gmail.com>
2024-06-03 02:30:33 +0700

    yt menit 30:02 main dan WP belom fix

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'E-Book',
+      debugShowCheckedModeBanner: false,
+      title: 'E Book',
       theme: lightTheme,
       home: const WelcomePage(),
     );
